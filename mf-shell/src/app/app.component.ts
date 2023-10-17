caa8bca47eb0c722dee2856d2acd8106ad814344
Jesús Araujo <jesusaraujo.0699@gmail.com>
2023-10-16 21:39:08 -0500

    Agregar estilos al mf-shell

--- mf-shell/src/app/app.component.ts
+++ mf-shell/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+interface ICommonProduct {
+  price: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mf-shell';
+  count = 0;
+  private _products: ICommonProduct[] = [];
+  
 }
